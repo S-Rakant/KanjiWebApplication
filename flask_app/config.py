3aeeda6e11efe81ab0d35f46f95a5c6d666fdfe7
S-Rakant <sousuke20020416@gmail.com>
2024-08-12 12:49:43 +0900

    modify config

--- flask_app/config.py
+++ flask_app/config.py
@@ -10,13 +10,13 @@ class Config:
 
     if not DEBUG:
         # SECRET_KEY設定
-        SECRET_KEY = os.environ('SECRET_KEY')
+        SECRET_KEY = os.environ['SECRET_KEY']
         # sqlalchemy設定
         SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
-            'user': os.environ('DB_USER'),
-            'password': os.environ('DB_PASSWORD'),
-            'host': os.environ('DB_HOST'),
-            'db_name': os.environ('DB_NAME'),
+            'user': os.environ['DB_USER'],
+            'password': os.environ['DB_PASSWORD'],
+            'host': os.environ['DB_HOST'],
+            'db_name': os.environ['DB_NAME'],
         })
         PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
         SQLALCHEMY_TRACK_MODIFICATIONS = False
